3748b579d4a8127da30bd818aec915461de038c8
archana srivastava <srivastavaarchana0991@gmail.com>
2020-03-17 16:39:20 -0500

    Fix rspec test and add formatter

--- spec/coding_bat_ruby_spec.rb
+++ spec/coding_bat_ruby_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'coding_bat_ruby/version'
+
 RSpec.describe CodingBatRuby do
   it 'has a version number' do
     expect(CodingBatRuby::VERSION).not_to be nil
