678e132abd874a12ba9728f8fe32abbc76325f29
ArchieAsthana <srivastavaarchana0991@gmail.com>
2020-03-19 13:00:49 -0500

    Add string_one exercises  (#2)
    * Add hello_name
    
    * Add without_two
    
    * Fixed formatting
    
    * updated to use range
    
    * Add con_cat
    
    * Add extra_front
    
    * Add front_again
    
    * Add middle_two
    
    * Fixed formatting issue
    
    * Add n_twice
    
    * Add without_end
    
    * Add see_color and see_color_nested_ternary
    
    * Add has_bad?
    
    * Add make_tags
    
    * Add without_x
    
    * Add first_half
    
    * Add without_x2

--- lib/coding_bat_ruby/string_one.rb
+++ lib/coding_bat_ruby/string_one.rb
@@ -3,16 +3,265 @@
 module CodingBatRuby
   # Basic String problems no loops
   class StringOne
-    # Given a string name, e.g. "Bob",
-    # return a greeting of the form "Hello Bob!".
+    # Given a string name, e.g. 'Bob',
+    # return a greeting of the form 'Hello Bob!'.
     #
     # Examples
     #
-    # helloName("Bob") -> "Hello Bob!"
-    # helloName("Alice") -> "Hello Alice!"
-    # helloName("X") -> "Hello X!"
+    # hello_name('Bob') -> 'Hello Bob!'
+    # hello_name('Alice') -> 'Hello Alice!'
+    # hello_name('X') -> 'Hello X!'
+    #
+    # Returns string
     def hello_name(name)
-      name
+      "Hello #{name}!"
+    end
+
+    # Given a string, if a length 2 substring appears at both its beginning and
+    # end, return a string without the substring at the beginning, so 'HelloHe'
+    # yields 'lloHe'. The substring may overlap with itself, so 'Hi' yields '.
+    # Otherwise, return the original string unchanged.
+    #
+    # Examples
+    #
+    # without_two('HelloHe') -> 'lloHe'
+    # without_two('HelloHi') -> 'HelloHi'
+    # without_two('Hi') -> ''
+    #
+    # Returns String
+    def without_two(str)
+      return str if str.length < 2
+      return '' if str.length == 2
+      return str[2..] if str[0...2].eql? str[-2..]
+
+      str
+    end
+
+    # Given two strings, append them together (known as 'concatenation') and
+    # return the result. However, if the concatenation creates a double-char,
+    # then omit one of the chars, so 'abc' and 'cat' yields 'abcat'.
+    #
+    # Examples
+    #
+    # con_cat('abc', 'cat') -> 'abcat'
+    # con_cat('dog', 'cat') -> 'dogcat'
+    # con_cat('abc', ') -> 'abc'
+    #
+    # Returns a String
+    def con_cat(value_a, value_b)
+      return value_a + value_b if value_a.empty? || value_b.empty?
+      return value_a[0...-1] + value_b if value_a[-1] == value_b[0]
+
+      value_a + value_b
+    end
+
+    # Given a string, return a new string made of 3 copies of the first 2 chars
+    # of the original string. The string may be any length. If there are fewer
+    # than 2 chars, use whatever is there.
+    #
+    # Examples
+    #
+    # extra_front('Hello') -> 'HeHeHe'
+    # extra_front('ab') -> 'ababab'
+    # extra_front('H') -> 'HHH'
+    #
+    # Returns a String
+    def extra_front(str)
+      return str if str.empty?
+
+      new_str = str.length < 2 ? str : str[0..1]
+      new_str * 3
+    end
+
+    # Given a string, return true if the first 2 chars in the string also
+    # appear at the end of the string, such as with 'edited'.
+    #
+    # Examples
+    #
+    # front_again('edited') -> true
+    # front_again('edit') -> false
+    # front_again('ed') -> true
+    #
+    # Returns a Boolean
+    def front_again(str)
+      str[0..1] == str[-2..]
+    end
+
+    # Given a string of even length, return a string made of the middle two
+    # chars, so the string 'string' yields 'ri'. The string length will be
+    # at least 2.
+    #
+    # Examples
+    #
+    # middle_two('string') -> 'ri'
+    # middle_two('code') -> 'od'
+    # middle_two('Practice') -> 'ct'
+    #
+    # Returns a String
+    def middle_two(str)
+      mid = str.length / 2
+      str[mid - 1...mid + 1]
+    end
+
+    # Given a string and an int n, return a string made of the first and last
+    # n chars from the string. The string length will be at least n.
+    #
+    # Examples
+    #
+    # n_twice('Hello', 2) -> 'Helo'
+    # n_twice('Chocolate', 3) -> 'Choate'
+    # n_twice('Chocolate', 1) -> 'Ce'
+    #
+    # Returns a String and Integer
+    def n_twice(str, value_n)
+      str[0...value_n] + str[str.length - value_n..]
+    end
+
+    # Given a string, return a version without the first and last char, so
+    # 'Hello' yields 'ell'. The string length will be at least 2.
+    #
+    # Examples
+    #
+    # without_end('Hello') -> 'ell'
+    # without_end('java') -> 'av'
+    # without_end('coding') -> 'odin'
+    #
+    # Returns a String
+    def without_end(str)
+      str[1...str.length - 1]
+    end
+
+    # Given a string, if the string begins with 'red' or 'blue' return that
+    # color string, otherwise return the empty string.
+    #
+    # Examples
+    #
+    # see_color('redxx') -> 'red'
+    # see_color('xxred') -> ''
+    # see_color('blueTimes') -> 'blue'
+    #
+    # Returns a String
+    def see_color(str)
+      return 'red' if str.start_with?('red')
+      return 'blue' if str.start_with?('blue')
+
+      ''
+    end
+
+    # Given a string, if the string begins with 'red' or 'blue' return that
+    # color string, otherwise return the empty string.
+    #
+    # Examples
+    #
+    # see_color_with_nested_ternary('redxx') -> 'red'
+    # see_color_with_nested_ternary('xxred') -> ''
+    # see_color_with_nested_ternary('blueTimes') -> 'blue'
+    #
+    # Returns a String
+    def see_color_with_nested_ternary(str)
+      # rubocop:disable Style/NestedTernaryOperator
+      str.start_with?('red') ? 'red' : str.start_with?('blue') ? 'blue' : ''
+      # rubocop:enable Style/NestedTernaryOperator
+    end
+
+    # Given a string, return true if 'bad' appears starting at index 0 or 1 in
+    # the string,such as with 'badxxx' or 'xbadxx' but not 'xxbadxx'.
+    # The string may be any length, including 0. Note: use .equals() to compare
+    # 2 strings.
+    #
+    # Examples
+    #
+    # has_bad?('badxx') -> true
+    # has_bad?('xbadxx') -> true
+    # has_bad?('xxbadxx') -> false
+    #
+    # Returns a Boolean
+    # rubocop:disable Naming/PredicateName
+    def has_bad?(str)
+      # rubocop:enable Naming/PredicateName
+      str[0...3].eql?('bad') || str[1...4].eql?('bad')
+    end
+
+    # The web is built with HTML strings like "<i>Yay</i>" which draws Yay as
+    # italic text. In this example, the "i" tag makes <i> and </i> which
+    # surround the word "Yay". Given tag and word strings, create the
+    # HTML string with tags around the word, e.g. "<i>Yay</i>".
+    #
+    # Examples
+    #
+    # make_tags('i', 'Yay') -> '<i>Yay</i>'
+    # make_tags('i', 'Hello') -> '<i>Hello</i>'
+    # make_tags('cite', 'Yay') -> '<cite>Yay</cite>'
+    #
+    # Returns a String
+    def make_tags(tag, word)
+      "<#{tag}>#{word}</#{tag}>"
+    end
+
+    # Given a string, if the first or last chars are 'x', return the string
+    # without Those 'x' chars, and otherwise return the string unchanged.
+    #
+    # Examples
+    #
+    # without_x('xHix') -> 'Hi'
+    # without_x('xHi') -> 'Hi'
+    # without_x('Hxix') -> 'Hxi'
+    #
+    # Returns a String
+    def without_x(str)
+      return str[1..-2] if str[0] == 'x' && str[-1] == 'x'
+      return str[1..] if str[0] == 'x'
+      return str[0..-2] if str[-1] == 'x'
+
+      str
+    end
+
+    # Given 2 strings, return their concatenation, except omit the first char
+    # of each. The strings will be at least length 1.
+    #
+    # Examples
+    #
+    # non_start('Hello', 'There') -> 'ellohere'
+    # non_start('java', 'code') -> 'avaode'
+    # non_start('shotl', 'java') -> 'hotlava'
+    #
+    # Returns a String
+    def non_start(value_a, value_b)
+      # value_a[1..-1] + value_b[1..-1]
+      value_a[1...value_a.length + 1] + value_b[1..-1]
+    end
+
+    # Given a string of even length, return the first half. So the string
+    # 'WooHoo' yields 'Woo'.
+    #
+    # Examples
+    #
+    # first_half('WooHoo') -> 'Woo'
+    # first_half('HelloThere') -> 'Hello'
+    # first_half('abcdef') -> 'abc'
+    #
+    # Returns a String
+    def first_half(str)
+      str[0...str.length / 2]
+    end
+
+    # Given a string, if one or both of the first 2 chars is 'x', return the
+    # string without those 'x' chars, and otherwise return the string unchanged
+    # This is a little harder than it looks.
+    #
+    # Examples
+    #
+    # without_x2('xHi') -> 'Hi'
+    # without_x2('Hxi') -> 'Hi'
+    # without_x2('Hi') -> 'Hi'
+    #
+    # Returns a String
+    def without_x2(str)
+      return str[2..-1] if str[0] == 'x' && str[1] == 'x'
+      return str[1..-1] if str[0] == 'x'
+      return str[0] + str[2..-1] if str[1] == 'x'
+
+      str
     end
   end
 end
---
+++ spec/coding_bat_ruby/string_one_spec.rb
@@ -0,0 +1,196 @@
+# frozen_string_literal: true
+
+require 'coding_bat_ruby/string_one'
+
+# rubocop:disable Metrics/BlockLength
+RSpec.describe CodingBatRuby::StringOne do
+  # rubocop:enable Metrics/BlockLength
+
+  it 'validates hello_name' do
+    expect(subject.hello_name('Bob')).to eq 'Hello Bob!'
+    expect(subject.hello_name('Alice')).to eq 'Hello Alice!'
+    expect(subject.hello_name('X')).to eq 'Hello X!'
+    expect(subject.hello_name('Dolly')).to eq 'Hello Dolly!'
+    expect(subject.hello_name('Alpha')).to eq 'Hello Alpha!'
+    expect(subject.hello_name('Omega')).to eq 'Hello Omega!'
+    expect(subject.hello_name('Goodbye')).to eq 'Hello Goodbye!'
+    expect(subject.hello_name('ho ho ho')).to eq 'Hello ho ho ho!'
+    expect(subject.hello_name('xyz!')).to eq 'Hello xyz!!'
+    expect(subject.hello_name('Hello')).to eq 'Hello Hello!'
+  end
+
+  it 'validates without_two' do
+    expect(subject.without_two('HelloHe')).to eq 'lloHe'
+    expect(subject.without_two('HelloHi')).to eq 'HelloHi'
+    expect(subject.without_two('Hi')).to eq ''
+    expect(subject.without_two('Chocolate')).to eq 'Chocolate'
+    expect(subject.without_two('xxx')).to eq 'x'
+    expect(subject.without_two('xx')).to eq ''
+    expect(subject.without_two('x')).to eq 'x'
+    expect(subject.without_two('')).to eq ''
+    expect(subject.without_two('Fruits')).to eq 'Fruits'
+  end
+
+  it 'validates con_cat' do
+    expect(subject.con_cat('abc', 'cat')).to eq 'abcat'
+    expect(subject.con_cat('dog', 'cat')).to eq 'dogcat'
+    expect(subject.con_cat('abc', '')).to eq 'abc'
+    expect(subject.con_cat('', 'cat')).to eq 'cat'
+    expect(subject.con_cat('pig', 'g')).to eq 'pig'
+    expect(subject.con_cat('pig', 'doggy')).to eq 'pigdoggy'
+  end
+
+  it 'validates extra_front' do
+    expect(subject.extra_front('Hello')).to eq 'HeHeHe'
+    expect(subject.extra_front('ab')).to eq 'ababab'
+    expect(subject.extra_front('H')).to eq 'HHH'
+    expect(subject.extra_front('')).to eq ''
+    expect(subject.extra_front('Candy')).to eq 'CaCaCa'
+    expect(subject.extra_front('Code')).to eq 'CoCoCo'
+  end
+
+  it 'validate front_again' do
+    expect(subject.front_again('edited')).to be true
+    expect(subject.front_again('edit')).to be false
+    expect(subject.front_again('ed')).to be true
+    expect(subject.front_again('jj')).to be true
+    expect(subject.front_again('jjj')).to be true
+    expect(subject.front_again('jjjk')).to be false
+    expect(subject.front_again('x')).to be false
+    expect(subject.front_again('')).to be false
+    expect(subject.front_again('java')).to be false
+    expect(subject.front_again('javaja')).to be true
+  end
+
+  it 'validates middle_two' do
+    expect(subject.middle_two('string')).to eq 'ri'
+    expect(subject.middle_two('code')).to eq 'od'
+    expect(subject.middle_two('Practice')).to eq 'ct'
+    expect(subject.middle_two('ab')).to eq 'ab'
+    expect(subject.middle_two('0123456789')).to eq '45'
+  end
+
+  it 'validates n_twice' do
+    expect(subject.n_twice('Hello', 2)).to eq 'Helo'
+    expect(subject.n_twice('Chocolate', 3)).to eq 'Choate'
+    expect(subject.n_twice('Chocolate', 1)).to eq 'Ce'
+    expect(subject.n_twice('Chocolate', 0)).to eq ''
+    expect(subject.n_twice('Hello', 4)).to eq 'Hellello'
+    expect(subject.n_twice('Code', 4)).to eq 'CodeCode'
+    expect(subject.n_twice('Code', 2)).to eq 'Code'
+  end
+
+  it 'validates without_end' do
+    expect(subject.without_end('Hello')).to eq 'ell'
+    expect(subject.without_end('java')).to eq 'av'
+    expect(subject.without_end('coding')).to eq 'odin'
+    expect(subject.without_end('code')).to eq 'od'
+    expect(subject.without_end('ab')).to eq ''
+    expect(subject.without_end('Chocolate!')).to eq 'hocolate'
+    expect(subject.without_end('Kitten')).to eq 'itte'
+    expect(subject.without_end('woohoo')).to eq 'ooho'
+  end
+
+  it 'validates see_color' do
+    expect(subject.see_color('redxx')).to eq 'red'
+    expect(subject.see_color('xxred')).to eq ''
+    expect(subject.see_color('blueTimes')).to eq 'blue'
+    expect(subject.see_color('NoColor')).to eq ''
+    expect(subject.see_color('red')).to eq 'red'
+    expect(subject.see_color('re')).to eq ''
+    expect(subject.see_color('blu')).to eq ''
+    expect(subject.see_color('blue')).to eq 'blue'
+    expect(subject.see_color('a')).to eq ''
+    expect(subject.see_color('')).to eq ''
+    expect(subject.see_color('xyzred')).to eq ''
+  end
+
+  it 'validates see_color_with_nested_ternary' do
+    expect(subject.see_color_with_nested_ternary('redxx')).to eq 'red'
+    expect(subject.see_color_with_nested_ternary('xxred')).to eq ''
+    expect(subject.see_color_with_nested_ternary('blueTimes')).to eq 'blue'
+    expect(subject.see_color_with_nested_ternary('NoColor')).to eq ''
+    expect(subject.see_color_with_nested_ternary('red')).to eq 'red'
+    expect(subject.see_color_with_nested_ternary('re')).to eq ''
+    expect(subject.see_color_with_nested_ternary('blu')).to eq ''
+    expect(subject.see_color_with_nested_ternary('blue')).to eq 'blue'
+    expect(subject.see_color_with_nested_ternary('a')).to eq ''
+    expect(subject.see_color_with_nested_ternary('')).to eq ''
+    expect(subject.see_color_with_nested_ternary('xyzred')).to eq ''
+  end
+
+  it 'validates has_bad?' do
+    expect(subject.has_bad?('badxx')).to be true
+    expect(subject.has_bad?('xbadxx')).to be true
+    expect(subject.has_bad?('xxbadxx')).to be false
+    expect(subject.has_bad?('code')).to be false
+    expect(subject.has_bad?('bad')).to be true
+    expect(subject.has_bad?('ba')).to be false
+    expect(subject.has_bad?('xba')).to be false
+    expect(subject.has_bad?('xbadx')).to be true
+    expect(subject.has_bad?('')).to be false
+    expect(subject.has_bad?('badyy')).to be true
+  end
+
+  it 'validates make_tags' do
+    expect(subject.make_tags('i', 'Yay')).to eq '<i>Yay</i>'
+    expect(subject.make_tags('i', 'Hello')).to eq '<i>Hello</i>'
+    expect(subject.make_tags('cite', 'Yay')).to eq '<cite>Yay</cite>'
+    expect(subject.make_tags('address', 'here')).to eq '<address>here</address>'
+    expect(subject.make_tags('body', 'Heart')).to eq '<body>Heart</body>'
+    expect(subject.make_tags('i', 'i')).to eq '<i>i</i>'
+    expect(subject.make_tags('i', '')).to eq '<i></i>'
+  end
+
+  it 'validates without_x' do
+    expect(subject.without_x('xHix')).to eq 'Hi'
+    expect(subject.without_x('xHi')).to eq 'Hi'
+    expect(subject.without_x('Hxix')).to eq 'Hxi'
+    expect(subject.without_x('Hi')).to eq 'Hi'
+    expect(subject.without_x('xxHi')).to eq 'xHi'
+    expect(subject.without_x('Hix')).to eq 'Hi'
+    expect(subject.without_x('xaxbx')).to eq 'axb'
+    expect(subject.without_x('xx')).to eq ''
+    expect(subject.without_x('x')).to eq ''
+    expect(subject.without_x('')).to eq ''
+    expect(subject.without_x('Hello')).to eq 'Hello'
+    expect(subject.without_x('Hexllo')).to eq 'Hexllo'
+  end
+
+  it 'validates non_start' do
+    expect(subject.non_start('Hello', 'There')).to eq 'ellohere'
+    expect(subject.non_start('java', 'code')).to eq 'avaode'
+    expect(subject.non_start('shoti', 'java')).to eq 'hotiava'
+    expect(subject.non_start('ab', 'xy')).to eq 'by'
+    expect(subject.non_start('ab', 'x')).to eq 'b'
+    expect(subject.non_start('x', 'ac')).to eq 'c'
+    expect(subject.non_start('a', 'x')).to eq ''
+    expect(subject.non_start('kit', 'kat')).to eq 'itat'
+    expect(subject.non_start('mart', 'dart')).to eq 'artart'
+  end
+
+  it 'validates first_half' do
+    expect(subject.first_half('WooHoo')).to eq 'Woo'
+    expect(subject.first_half('HelloThere')).to eq 'Hello'
+    expect(subject.first_half('abcdef')).to eq 'abc'
+    expect(subject.first_half('ab')).to eq 'a'
+    expect(subject.first_half('')).to eq ''
+    expect(subject.first_half('0123456789')).to eq '01234'
+    expect(subject.first_half('kitten')).to eq 'kit'
+  end
+
+  it 'validates without_x2' do
+    expect(subject.without_x2('xHi')).to eq 'Hi'
+    expect(subject.without_x2('Hxi')).to eq 'Hi'
+    expect(subject.without_x2('Hi')).to eq 'Hi'
+    expect(subject.without_x2('xxHi')).to eq 'Hi'
+    expect(subject.without_x2('Hix')).to eq 'Hix'
+    expect(subject.without_x2('xaxb')).to eq 'axb'
+    expect(subject.without_x2('xx')).to eq ''
+    expect(subject.without_x2('x')).to eq ''
+    expect(subject.without_x2('')).to eq ''
+    expect(subject.without_x2('Hello')).to eq 'Hello'
+    expect(subject.without_x2('Hexllo')).to eq 'Hexllo'
+    expect(subject.without_x2('xHxllo')).to eq 'Hxllo'
+  end
+end
